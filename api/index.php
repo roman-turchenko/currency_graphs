1e3104b2a49353dea69a519d26a1dc9fab5e7c18
roman.turchenko <poman-t@yandex.ru>
2016-09-07 20:03:39 +0300

    Basic functionality were added

--- api/index.php
+++ api/index.php
@@ -6,15 +6,29 @@
  * Time: 0:35
  */
 
+// www.cbr.ru/scripts/XML_dynamic.asp?date_req1=02/03/2001&date_req2=14/03/2001&VAL_NM_RQ=R01235
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "www.cbr.ru/scripts/XML_dynamic.asp?date_req1=02/03/2001&date_req2=14/03/2001&VAL_NM_RQ=R01235");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$xml_string = curl_exec($ch);
-curl_close($ch);
+$currencyCode = $_GET['currencyCode'] ? $_GET['currencyCode'] : '';
+$startDate = $_GET['startDate'] ? $_GET['startDate'] : '';
+$endDate = $_GET['endDate'] ? $_GET['endDate'] : '';
 
-$xml = simplexml_load_string($xml_string);
+if (!empty($currencyCode) && !empty($startDate) && !empty($endDate)){
+
+    $requestURL = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=".$startDate."&date_req2=".$endDate."&VAL_NM_RQ=".$currencyCode;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $requestURL);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $xml_string = curl_exec($ch);
+    curl_close($ch);
+
+    $result = simplexml_load_string($xml_string);
+}
+else{
+
+    $result = array("result" => false, "message" => "Some vars came empty", "get" => $_GET);
+}
 
 header("Content-type: application/json");
-print json_encode($xml);
+print json_encode($result);
